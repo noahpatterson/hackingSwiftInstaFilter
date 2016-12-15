a88516a98858c64894b628c2f6b7fdd4c2e3d1a8
noahpatterson <n@noahpatterson.me>
2016-12-15 11:09:30 -0700

    create layout, add actions and outlets

--- InstaFilter/ViewController.swift
+++ InstaFilter/ViewController.swift
@@ -9,7 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var intensity: UISlider!
 
+    @IBAction func changeFilter() {
+    }
+    @IBAction func save() {
+    }
+    @IBAction func intensityChanged(_ sender: UISlider) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
