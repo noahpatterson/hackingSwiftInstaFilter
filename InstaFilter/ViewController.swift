22fa22f432fe3a1a99d31a382a40ebefe202b385
noahpatterson <n@noahpatterson.me>
2016-12-15 17:15:53 -0700

    allow changing sepia filter with slider
    + embed in navigation controller
    + add authorization to info.plist for access to photo library

--- InstaFilter/ViewController.swift
+++ InstaFilter/ViewController.swift
@@ -7,24 +7,33 @@
 //
 
 import UIKit
+import CoreImage
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var intensity: UISlider!
     
     var currentImage: UIImage!
+    var context: CIContext!
+    var currentFilter: CIFilter!
 
     @IBAction func changeFilter() {
     }
+    
     @IBAction func save() {
     }
+    
     @IBAction func intensityChanged(_ sender: UISlider) {
+        applyProcessing()
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         title = "YACIFP"
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
+        context = CIContext()
+        currentFilter = CIFilter(name: "CISepiaTone")
     }
 
     override func didReceiveMemoryWarning() {
@@ -36,6 +45,11 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         guard let image = info[UIImagePickerControllerEditedImage] as? UIImage else { return }
         dismiss(animated: true)
         currentImage = image
+        
+        let beginImage = CIImage(image: currentImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        
+        applyProcessing()
     }
 
     func importPicture() {
@@ -43,6 +57,17 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         picker.allowsEditing = true
         picker.delegate = self
         present(picker, animated: true)
+    }
+    
+    func applyProcessing() {
+        currentFilter.setValue(intensity.value, forKey: kCIInputIntensityKey)
+        // `currentFilter.outputImage!.extent` -- means render all of it 
+            //-- until this method is called no actual processing is done
+        if let cgimg = context.createCGImage(currentFilter.outputImage!, from: currentFilter.outputImage!.extent) {
+            let processedImage = UIImage(cgImage: cgimg)
+            imageView.image = processedImage
+        }
+        
     }
 }
 
